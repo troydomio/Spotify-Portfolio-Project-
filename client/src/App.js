a9746f155aa4f55e655bc994f8166b72f6cb9e56
Troy <troyakins99@yahoo.com>
2021-09-07 17:19:59 -0500

    Created front-end of application via react

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+        >
+          Login to Spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
