dbbeddef1d614033593ab73f2b5cb23ede8d35f3
Troy <troyakins99@yahoo.com>
2021-09-07 16:34:44 -0500

    added callback route

--- index.js
+++ index.js
@@ -22,17 +22,41 @@ app.get('/', (req, res) => {
 // app.get('/callback', (req,res) => {
 
 // })
-    const queryParams = querystring.stringify({
-        client_id: CLIENT_ID,
-        response_type: 'code',
-        redirect_uri: REDIRECT_URI
-    })
 
-    app.get('/login', (req, res) => {
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+   /**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  const stateKey = 'spotify_auth_state';
   
+    app.get('/login', (req, res) => {
+        const state = generateRandomString(16);
+        res.cookie(stateKey, state);
+      
+        const scope = 'user-read-private user-read-email';
+
+        const queryParams = querystring.stringify({
+            client_id: CLIENT_ID,
+            response_type: 'code',
+            redirect_uri: REDIRECT_URI,
+            state: state,
+            scope: scope,
+        });
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
+app.get('/callback',(req,res)=> {
+    res.send('callback')
+})
 
 app.listen(port,()=>{
     console.log(`Express app is listening at http://localhost:${port}`)
