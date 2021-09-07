c3b1ac858ce3deea214927602da81ddc224a9b83
Troy <troyakins99@yahoo.com>
2021-09-07 17:29:56 -0500

    Connected react app to the bakend using redirects

--- index.js
+++ index.js
@@ -71,49 +71,20 @@ app.get('/callback', (req, res) => {
         Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
       },
     })
-    .then(response => {
-        if (response.status === 200) {
-    
-          const { access_token, token_type } = response.data;
-    
-          axios.get('https://api.spotify.com/v1/me', {
-            headers: {
-              Authorization: `${token_type} ${access_token}`
-            }
-          })
-            .then(response => {
-              res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-            })
-            .catch(error => {
-              res.send(error);
-            });
-    
-        } else {
-          res.send(response);
-        }
-      })
-      .catch(error => {
-        res.send(error);
-      });
-  });
-
-  app.get('/refresh_token', (req, res) => {
-    const { refresh_token } = req.query;
-  
-    axios({
-      method: 'post',
-      url: 'https://accounts.spotify.com/api/token',
-      data: querystring.stringify({
-        grant_type: 'refresh_token',
-        refresh_token: refresh_token
-      }),
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-      },
-    })
       .then(response => {
-        res.send(response.data);
+        if (response.status === 200) {
+          const { access_token, refresh_token } = response.data;
+  
+          const queryParams = querystring.stringify({
+            access_token,
+            refresh_token,
+          });
+  
+          res.redirect(`http://localhost:3000/?${queryParams}`);
+  
+        } else {
+          res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
+        }
       })
       .catch(error => {
         res.send(error);
